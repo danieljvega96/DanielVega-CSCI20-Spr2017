075194ed0087c43764d6478956752d4c9d3a0be4
Daniel Vega <dvega011@student.butte.edu>
2017-01-27 22:29:05 +0000

    Lab1.1submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,9 +1,16 @@
 /*  This is a guessing game in which the user is given 10 trys to guess a randomly
-generated number by the computer.
-First, the computer will randomly pick one number between 1 and 1000, the user 
-must attempt to guess this number. If the user enters a number lower than the 
-randomly generated number, the phrase "higher" will be displayed. If the user 
-enters a number higher than the randomly generated number, the phrase "lower"
-will be displayed.
-*/
+generated number by the computer. First, the computer will randomly pick one 
+number between 1 and 1000, and will show the phrase "Guess a number between 1 and 1000",
+after this, the user will be allowed to input a number between 1 and 1000. If 
+the user enters a number lower than the randomly generated number, the phrase 
+"higher" will be displayed. If the user  enters a number higher than the randomly
+generated number, the phrase "lower" will be displayed. For every incorrect guess,
+the user gets a point subtracted from their score and the phrase "Lower/Higher" 
+will be displayed, depending on the user's guess, followed by (however many 
+points are left) "more trys". After the user's points are reduced to zero,
+the phrase "Sorry, you lost, the number was" followed by the randomly generated
+number. If the user guesses the correct number, the phrase "congratulations! That
+was the correct number!" Will be displayed, followed by the phrase "Your points
+are" followed by the amount of points left.
 
+*/
