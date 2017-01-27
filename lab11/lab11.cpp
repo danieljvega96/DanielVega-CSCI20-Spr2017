7062684b7d4962abf7cdf11cc1322c0310619829
Daniel Vega <dvega011@student.butte.edu>
2017-01-27 00:28:01 +0000

    lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,9 @@
+/*  This is a guessing game in which the user is given 10 trys to guess a randomly
+generated number by the computer.
+First, the computer will randomly pick one number between 1 and 1000, the user 
+must attempt to guess this number. If the user enters a number lower than the 
+randomly generated number, the phrase "higher" will be displayed. If the user 
+enters a number higher than the randomly generated number, the phrase "lower"
+will be displayed.
+*/
+
