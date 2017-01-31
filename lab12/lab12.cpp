54aa12c59160fb83c65f1f22500e1d132270b361
Daniel Vega <dvega011@student.butte.edu>
2017-01-31 22:46:28 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    
+    cout << "    __//" << endl;
+    cout << "  /o__o\\" << endl;
+    cout << "  \\    /" << endl;
+    cout << "__/    \\" << endl;
+    cout << "\\       )" << endl;
+    cout << " \\_____/" << endl;
+    cout << "__|___|__ " << endl;
+}
