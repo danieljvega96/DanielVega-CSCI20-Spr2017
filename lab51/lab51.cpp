11407e13341cd1336b6afe2eedcc5aa636480e2c
Daniel Vega <dvega011@student.butte.edu>
2017-05-04 22:44:36 +0000

    lab5.1 submission

---
+++ lab51/lab51.cpp
@@ -0,0 +1,66 @@
+#include <iostream> 
+#include <iostream> 
+#include <cstring> 
+using namespace std;   
+
+int main() {
+    int nameLength = 0;
+    
+    //array was not given a set number of indexies
+    char * name = new char[];
+
+    cout << "Please enter a 10 letter word or less" << endl;
+    cin >> name;
+
+    char * head  = name;
+    char * tail = name;
+    
+    //can not use a string mutation function 
+    nameLength = strlen(*name);
+
+    cout << "Your word is " << firststr << endl;
+
+    if (nameLength<10)
+    {
+         while (*head != '\0')
+         {
+                cout << *head;
+                head++;
+         }
+     }
+     else
+     {
+          cout << "WARNING WORD TOO BIG TO DISPLAY!!" << endl;
+     }
+
+     cout << endl;
+
+     tail = &name[strlen(name) - 1];
+
+     if (nameLength < 10)
+     {
+          while (tail != name)
+          {
+                 cout << *tail;
+                 tail--;
+          }
+     }
+     cout << endl;
+
+     head = name;
+     tail = &name[strlen(name) - 1];
+
+     head++;
+     tail--;
+
+     if (*head == *tail)
+     {
+         cout << "It is an palindrome!" << endl;
+     }
+     else
+     {
+         cout << "It is not an palindrome" << endl;
+     }
+
+     return 0; 
+}
